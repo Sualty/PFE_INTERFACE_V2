681eebb3a819981e2e60bc7fd69ac8f2f5cdfa5d
Sualty <lagouge@polytech.unice.fr>
2017-02-21 13:52:53 +0100

    connexion et récup de la rasp

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.jcraft.jsch.JSchException;
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
 import javafx.application.Platform;
@@ -11,7 +12,9 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import javafx.event.ActionEvent;
+import tools.network.ConnectorPi;
 
+import java.io.IOException;
 import java.util.concurrent.*;
 
 public class Controller {
@@ -24,11 +27,15 @@ public class Controller {
 
     private boolean isLaunchingAcq;
 
+    private ConnectorPi connectorPi;
+
     @FXML
     public void initialize() {
         System.out.println("initializing controller");
 
         isLaunchingAcq = false;
+
+        connectorPi = new ConnectorPi();
 
         timer_textfield.textProperty().addListener(new ChangeListener<String>() {
             @Override
@@ -56,10 +63,13 @@ public class Controller {
     }
 
     public void connectToRPI(ActionEvent actionEvent) {
-        //TODO
+        try {
+            connectorPi.connexionPi();
+        } catch (JSchException e) {
+            e.printStackTrace();
+        }
     }
 
-//TODO launch connection at 1 second
     public void launchAcq(ActionEvent actionEvent) {
         if(!isLaunchingAcq) {
             launch_acq_button.getStyleClass().add("busy");
@@ -74,19 +84,20 @@ public class Controller {
 
                     for (int i = time; i >= 0; i--) {
                         final int j = i;
-                        CallableVoice callableVoice = new CallableVoice(j);
-                        CallableLabel callableLabel = new CallableLabel(j);
-
-                        FutureTask<String> futureTask1 = new FutureTask<String>(callableVoice);
-                        FutureTask<String> futureTask2 = new FutureTask<String>(callableLabel);
-
-                        ExecutorService executor = Executors.newFixedThreadPool(2);
-
-                        executor.execute(futureTask1);
-                        executor.execute(futureTask2);
-
+                        timer(j);
                         try {
-                            Thread.sleep(1000);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        if(i==1) {
+                          Thread thread_launch = new Thread(new LaunchingThread());
+                          thread_launch.start();
+
+                            System.out.println("launcheeeeeeeeeeeeeeeeeeeeeeeeed");
+                        }
+                        try {
+                            Thread.sleep(500);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
@@ -106,7 +117,7 @@ public class Controller {
         }
     }
 
-    public void sayNumber(int i) {
+    private void sayNumber(int i) {
         VoiceManager vm = VoiceManager.getInstance();
         Voice voice = vm.getVoice("kevin16");
 
@@ -115,10 +126,22 @@ public class Controller {
         voice.speak(i+"");
     }
 
+    private void timer(int j) {
+        CallableVoice callableVoice = new CallableVoice(j);
+        CallableLabel callableLabel = new CallableLabel(j);
+
+        FutureTask<String> futureTask1 = new FutureTask<String>(callableVoice);
+        FutureTask<String> futureTask2 = new FutureTask<String>(callableLabel);
+
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        executor.execute(futureTask1);
+        executor.execute(futureTask2);
+    }
 
     private class CallableVoice implements Callable<String> {
         int j;
-        public CallableVoice(int j) {
+        CallableVoice(int j) {
             this.j = j;
         }
         @Override
@@ -131,7 +154,7 @@ public class Controller {
 
     private class CallableLabel implements Callable<String> {
         int j;
-        public CallableLabel(int j) {
+        CallableLabel(int j) {
             this.j = j;
         }
         @Override
@@ -144,6 +167,20 @@ public class Controller {
             });
             //return the thread name executing this callable task
             return Thread.currentThread().getName();
+        }
+    }
+
+    private class LaunchingThread implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                connectorPi.lancementAcquisition();
+            } catch (JSchException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
